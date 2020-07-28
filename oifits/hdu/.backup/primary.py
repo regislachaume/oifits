94bfda88924e7739e6d92e9395d76f8bee4ac77d
loqueelvientoajuarez <regis.lachaume@gmail.com>
2020-07-28 17:55:51 -0400

    first draft

--- oifits/hdu/.backup/primary.py
+++
@@ -1,8 +0,0 @@
-from astropy.io import fits
-
-class PrimaryHDU(fits.PrimaryHDU):
-    def __repr__(self):
-
-        if self.data:
-            return f"<PrimaryHDU ({'×'.join(self.data.shape)} image)>"
-        return '<PrimaryHDU>'
